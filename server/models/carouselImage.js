89af22ca079791be26bb7360ef1aaf67ea53c762
Julia Guterres Pellin <juuhp2008@gmail.com>
2026-08-11 01:07:50 -0300

    mais frufrus e carrossel funcionando

--- server/models/carouselImage.js
+++ server/models/carouselImage.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const carouselImageSchema = new mongoose.Schema({
   slot: {
     type: String,
-    enum: ['home', 'login', 'register'],
+    enum: ['home', 'login', 'register', 'auth'],
     required: true,
   },
   url: {
